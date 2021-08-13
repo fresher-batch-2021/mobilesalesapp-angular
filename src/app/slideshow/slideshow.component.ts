88c4057ea255e4572e08fd221cf8c155184dbada
Bharathi kannan <baranibk13299@gmail.com>
2021-08-13 13:07:34 +0530

    files modified

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -14,21 +14,21 @@ export class SlideshowComponent implements OnInit {
 
   slideIndex:any = 0;
 
-// showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   this.slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[this.slideIndex - 1].style.display = "block";
-//   dots[this.slideIndex - 1].className += " active";
-//   setTimeout(showSlides, 3000);
-// }
+showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  this.slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[this.slideIndex - 1].style.display = "block";
+  dots[this.slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000);
+}
 
 }
